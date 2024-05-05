c380da70e33dd1f055188a78e786647953f80d3c
PRATAP-KUMAR <pratap@fastmail.fm>
2024-05-05 12:27:18 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,26 +12,33 @@ function Login() {
     }
 
     return (
-        <form onSubmit={handleSubmit} className='flex flex-col justify-start space-y-5 items-center mt-10 max-w-4xl mx-auto bg-green-200'>
+        <div className="flex flex-col min-h-[calc(100vh-4rem)] items-center justify-center 2xl:flex-row border-2 border-red-500">
+            <div className='flex bg-cyan-500 flex-col items-center h-1/2 border-2 border-red-500 justify-center'>
+                <img
+                    className="w-96 border-2  border-green-500"
+                    src="images/Wavy_Gen-01_Single-07.jpg"
+                    alt="Login Photo"
+                />
+                <a href="https://www.freepik.com/free-vector/user-verification-unauthorized-access-prevention-private-account-authentication-cyber-security-people-entering-login-password-safety-measures_12146011.htm#query=login&position=6&from_view=keyword&track=sph&uuid=4e1235ce-aea3-478b-a2ae-0ab3e2b4f72f" className='text-red-500'>Image by vectorjuice</a> on Freepik
+            </div>
+            <form onSubmit={handleSubmit} className='h-1/2 max-w-7xl 2xl:min-h-[calc(100vh-4rem)] flex flex-col justify-start p-10 w-full bg-light 2xl:w-2/3 space-y-5 items-center mx-auto bg-green-200'>
+                <input
+                    type='email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    placeholder='Email'
+                />
 
-            <h3 className='font-bold'>Login</h3>
-
-            <label>Email: </label>
-            <input
-                type='email'
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
-
-            <label>Password: </label>
-            <input
-                type='password'
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
-            <button disabled={isLoading} className='btn-blue'>Login</button>
-            {error && <div>{error}</div>}
-        </form>
+                <input
+                    type='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    placeholder='Password'
+                />
+                <button disabled={isLoading} className='btn-blue'>Login</button>
+                {error && <div>{error}</div>}
+            </form>
+        </div>
     )
 }
 
