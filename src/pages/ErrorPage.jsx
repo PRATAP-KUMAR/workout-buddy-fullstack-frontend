1e003a8366e259261ac0cf46995c38c4b2f9f19f
PRATAP-KUMAR <pratap@fastmail.fm>
2024-05-09 07:08:20 +0530

    deployed with netlify

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function ErrorPage() {
     return (
-        <div className='min-h-[calc(100vh-4rem)] font-bold text-2xl text-center justify-center items-center bg-lite text-toodark flex'>
-            Oops! Sorry, there seems to be a Network Error/Backend is not connected.
+        <div className='min-h-[calc(100vh-4rem)] font-bold text-2xl text-center justify-center flex-col items-center bg-lite text-toodark flex'>
+            <p>Oops! Sorry, there seems to be a Network Error/Backend is not connected.</p>
+            <a
+                href='https://github.com/PRATAP-KUMAR/workout-mern-backend.git'
+                target='_blank'
+                rel='noreferrer'
+                className='text-blue-500'
+            >
+                Backend Instructions
+            </a>
         </div>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,7 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const { login, error, isLoading } = useLogin();
+    const [loading, setLoading] = useState(null);
 
     const [Error, setError] = useState(null);
 
@@ -17,10 +18,13 @@ function Login() {
     useEffect(() => {
         const checkBackend = async () => {
             try {
+                setLoading(true);
                 const response = await fetch('http://localhost:4000/api/test');
                 const json = await response.json();
                 console.log(json.message);
+                setLoading(false);
             } catch (error) {
+                setLoading(false);
                 setError(error);
             }
         }
@@ -30,44 +34,46 @@ function Login() {
     return (
         <>
             {
-                Error ? <ErrorPage />
+                loading ? <div>Loading...</div>
                     :
-                    <div className="flex flex-col min-h-[calc(100vh-4rem)] items-center justify-start 2xl:flex-row">
-                        <div className='flex flex-col items-center justify-center 2xl:w-1/3'>
-                            <img
-                                className="w-96"
-                                src="images/Wavy_Gen-01_Single-07.svg"
-                                alt="Login Photo"
-                            />
-                            <a
-                                className='text-blue-500 text-xs'
-                                target="_blank"
-                                href='https://www.freepik.com/free-vector/user-verification-unauthorized-access-prevention-private-account-authentication-cyber-security-people-entering-login-password-safety-measures_12146011.htm#query=login&position=7&from_view=keyword&track=sph&uuid=5f7de01f-1a23-4a2d-9039-04035ff6a96d#page=1&query=l&from_query=undefined&position=0&from_view=keyword&track=sph&uuid=5f7de01f-1a23-4a2d-9039-04035ff6a96d'
-                                rel="noreferrer"
-                            >
-                                Image by vectorjuice on Freepik
-                            </a>
-                        </div>
-                        <form onSubmit={handleSubmit} className='flex-1 2xl:min-h-[calc(100vh-4rem)] flex flex-col w-full bg-dark px-12 2xl:w-2/3 space-y-5 items-center justify-center mx-auto'>
-                            <h1 className='text-2xl font-bold text-toodark'>Login to Workout Buddy</h1>
-                            <input
-                                type='email'
-                                onChange={(e) => setEmail(e.target.value)}
-                                value={email}
-                                placeholder='Email'
-                                className='w-full h-12 pl-2 focus:ring-1 focus:ring-toodark caret-toodark placeholder-dark'
-                            />
-                            <input
-                                type='password'
-                                onChange={(e) => setPassword(e.target.value)}
-                                value={password}
-                                placeholder='Password'
-                                className='w-full h-12 pl-2 focus:ring-1 focus:ring-toodark caret-toodark placeholder-dark'
-                            />
-                            <button disabled={isLoading} className='button'>Login</button>
-                            {error && <div className='text-red-500'>{error}</div>}
-                        </form>
-                    </div >
+                    Error ? <ErrorPage />
+                        :
+                        <div className="flex flex-col min-h-[calc(100vh-4rem)] items-center justify-start 2xl:flex-row">
+                            <div className='flex flex-col items-center justify-center 2xl:w-1/3'>
+                                <img
+                                    className="w-96"
+                                    src="images/Wavy_Gen-01_Single-07.svg"
+                                    alt="Login Photo"
+                                />
+                                <a
+                                    className='text-blue-500 text-xs'
+                                    target="_blank"
+                                    href='https://www.freepik.com/free-vector/user-verification-unauthorized-access-prevention-private-account-authentication-cyber-security-people-entering-login-password-safety-measures_12146011.htm#query=login&position=7&from_view=keyword&track=sph&uuid=5f7de01f-1a23-4a2d-9039-04035ff6a96d#page=1&query=l&from_query=undefined&position=0&from_view=keyword&track=sph&uuid=5f7de01f-1a23-4a2d-9039-04035ff6a96d'
+                                    rel="noreferrer"
+                                >
+                                    Image by vectorjuice on Freepik
+                                </a>
+                            </div>
+                            <form onSubmit={handleSubmit} className='flex-1 2xl:min-h-[calc(100vh-4rem)] flex flex-col w-full bg-dark px-12 2xl:w-2/3 space-y-5 items-center justify-center mx-auto'>
+                                <h1 className='text-2xl font-bold text-toodark'>Login to Workout Buddy</h1>
+                                <input
+                                    type='email'
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                    placeholder='Email'
+                                    className='w-full h-12 pl-2 focus:ring-1 focus:ring-toodark caret-toodark placeholder-dark'
+                                />
+                                <input
+                                    type='password'
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    value={password}
+                                    placeholder='Password'
+                                    className='w-full h-12 pl-2 focus:ring-1 focus:ring-toodark caret-toodark placeholder-dark'
+                                />
+                                <button disabled={isLoading} className='button'>Login</button>
+                                {error && <div className='text-red-500'>{error}</div>}
+                            </form>
+                        </div >
             }
         </>
     )
