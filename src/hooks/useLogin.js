87b7003e1b901a1ad9618bbefb2e66d14b4a6e69
PRATAP-KUMAR <pratap@fastmail.fm>
2024-05-13 15:43:57 +0530

    update API

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import API from "../../api";
 
 export const useLogin = () => {
     const [error, setError] = useState(null);
@@ -11,7 +12,7 @@ export const useLogin = () => {
         setError(null);
 
         try {
-            const response = await fetch('http://localhost:4000/api/user/login', {
+            const response = await fetch(`${API}/api/user/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
